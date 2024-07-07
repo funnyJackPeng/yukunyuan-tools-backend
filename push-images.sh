9d435d4ea1d9a3c616c9d6722c7d5df595a78924
funnyjack <554517318@qq.com>
2024-07-07 20:29:21 +0800

    修改 push 脚本

--- push-images.sh
+++ push-images.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 set -e
-docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME/email-service:$IMAGE_TAG
-docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME/migration-job:$IMAGE_TAG
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
