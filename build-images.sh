60c995cf59a039d6a7084613f44d72650729c6b5
funnyjack <554517318@qq.com>
2024-07-07 20:21:33 +0800

    编写 push image 脚本

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -e
 ./gradlew email-service:bootBuildImage
+docker tag email-service:0.0.1-SNAPSHOT $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME/email-service:$IMAGE_TAG
 ./gradlew migration-job:bootBuildImage
+docker tag migration-job:0.0.1-SNAPSHOT $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME/migration-job:$IMAGE_TAG
 #./gradlew monolith-service:bootBuildImage
