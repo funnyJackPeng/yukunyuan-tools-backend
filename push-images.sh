5545bb3bf49a8c037d3ed67d7adfb18839c4b680
funnyJack <554517318@qq.com>
2024-07-09 11:19:55 +0800

    添加 image_list artifact

--- push-images.sh
+++ push-images.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 images=$(docker images "*.dkr.ecr.*.amazonaws.com/*:latest" --format "{{.Repository}}")
+echo "write image list to txt file"
+images > /image_list.txt
 echo "$images" | while read -r image ; do
   echo "$image"
   docker push "$image"
