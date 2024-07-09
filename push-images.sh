15c4f17683367bae952b67e2a85dfd44148fe96a
funnyJack <554517318@qq.com>
2024-07-09 15:14:58 +0800

    指定输出的 json 文件的目录

--- push-images.sh
+++ push-images.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -e
 # 创建一个空的 JSON 数组
-echo "[" > imagedefinitions.json
+echo "[" > /imagedefinitions.json
 
 # 获取所有 ECR 镜像的 URI 并写入 JSON 文件
 docker images "*.dkr.ecr.*.amazonaws.com/*:latest" --format "{{.Repository}}" | while read -r image; do
-  echo "{\"name\": \"${image##*/}\", \"imageUri\": \"$image:latest\"}" >> imagedefinitions.json
+  echo "{\"name\": \"${image##*/}\", \"imageUri\": \"$image:latest\"}" >> /imagedefinitions.json
   # 推送镜像到 ECR
   docker push "$image"
 done
 
 # 补全 JSON 数组
-echo "]" >> imagedefinitions.json
+echo "]" >> /imagedefinitions.json
