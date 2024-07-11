d50fdbd1f7e3318b133bff98a4a5e1fce6e76a81
funnyJackPeng <13388883711@163.com>
2024-07-12 00:08:35 +0800

    修改 push-images 的 shell，正确写入 json 文件

--- push-images.sh
+++ push-images.sh
@@ -4,10 +4,18 @@ set -e
 echo "[" > /imagedefinitions.json
 
 # 获取所有 ECR 镜像的 URI 并写入 JSON 文件
-docker images "*.dkr.ecr.*.amazonaws.com/*:latest" --format "{{.Repository}}" | while read -r image; do
+images=$(docker images "*.dkr.ecr.*.amazonaws.com/*:latest" --format "{{.Repository}}")
+image_count=$(echo "$images" | wc -l)
+i=1
+
+echo "$images" | while read -r image; do
   echo "{\"name\": \"${image##*/}\", \"imageUri\": \"$image:latest\"}" >> /imagedefinitions.json
   # 推送镜像到 ECR
   docker push "$image"
+  if [ $i -lt $image_count ]; then
+    echo "," >> /imagedefinitions.json
+  fi
+  i=$((i+1))
 done
 
 # 补全 JSON 数组
